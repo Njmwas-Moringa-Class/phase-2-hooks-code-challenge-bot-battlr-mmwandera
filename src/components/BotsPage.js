735051c4332019469b678e90891fb34db746238c
mmwandera <mmwandera74@gmail.com>
2023-12-04 23:58:53 +0300

    completed code challenge

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
 
 function BotsPage() {
   //start here with your code for step one
+ const [botCollection, setBotCollection] = React.useState([]);
 
+  useEffect(() => {
+    fetch("http://localhost:8002/bots")
+      .then((res) => res.json())
+      .then((data) => setBotCollection(data))
+  })
+  
   return (
     <div>
       <YourBotArmy />
